dcfac9e2654f69cd861239db3a103a92844e8df8
Joel Hill <joel.hill@usask.ca>
2016-03-31 20:23:38 -0600

    Started classes controller. Gets student and all classes

--- classesApp.js
+++ classesApp.js
@@ -1,9 +1,17 @@
-(function(){
-    var app = angular.module('classes', []);
-    app.controller('classes', function(){
-        this.output = function(){
-            console.log("From other controller: "+this.data.name);    
-        };
+ app.controller('classes', ['$http', '$scope', function($http, $scope){
+    $scope.classes = [];
+    $scope.studentClasses = [];
+    $http.get(apiBase + '/classes').success(function(r){
+        $scope.classes = r;
     });
+    $scope.$watch('user', function(newV, old){
+        if(newV["student"] != null){
+            //console.log("User updated: "+newV["student"]["studentID"]);
+            $http.get(apiBase + 'students/' + newV["student"]["studentID"] + '/classes').then(function(r){
+                $scope.studentClasses = r.data;
+            });
+        }
+    });    
 
-})();
+}]);
+
